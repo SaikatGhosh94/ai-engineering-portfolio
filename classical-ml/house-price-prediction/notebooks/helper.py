724509fc0db631057137fe43f87f9d310e1e34e2
SaikatGhosh94 <official.ghosh.saikat@gmail.com>
2026-01-31 18:43:23 +0530

    house-price-prediction : added feature engineering for train set

---
+++ classical-ml/house-price-prediction/notebooks/helper.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+def percent_missing(df):
+    percent_nan = 100 * df.isnull().sum() /len(df)
+    percent_nan = percent_nan[percent_nan > 0].sort_values()
+    return percent_nan
